e1eb3de33d3e46df3b978d2aee0c9a50e042f432
claudiaps <clp.sampedro@gmail.com>
2023-01-24 20:28:39 -0300

    feat: update App.js text and style

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit code one more time
+          Alterando para a branch develop
         </p>
         <a
           className="App-link"
